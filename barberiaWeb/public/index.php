47c76f6d0bd912f9ed0689f06baf0475f9c739e2
sebastiandayekh04 <156692475+sebastiandayekh04@users.noreply.github.com>
2025-02-20 18:20:57 +0000

    Intento de ir creando el footer, header y main barberNation v1.0

--- barberiaWeb/public/index.php
+++ barberiaWeb/public/index.php
@@ -3,9 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BarberNationBCN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="../assets/css/style.css">
 </head>
 <body>
-    
+<?php require_once 'views/header.php' ; 
+?>  
+
+    <main>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-md-4 mb-4">
+                <div class="card bg-dark text-white h-100">
+                    <img src="https://i.pinimg.com/736x/43/57/07/435707ccf07d44e5a308aab52bf1826f.jpg" class="card-img" alt="corte prueba">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Card Title 1</h5>
+                        <p class="card-text">A short description of this card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 mb-4">
+                <div class="card bg-dark text-white h-100">
+                    <img src="https://i.pinimg.com/736x/7f/f4/16/7ff416263a6844f75acddd0b1aff86bb.jpg" class="card-img" alt="corte prueba">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Card Title 2</h5>
+                        <p class="card-text">Another example with some additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4 mb-4">
+                <div class="card bg-dark text-white h-100">
+                    <img src="https://i.pinimg.com/736x/fe/39/d0/fe39d0b9e865a14faacbf161f1eae783.jpg" class="card-img" alt="corte prueba">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Card Title 3</h5>
+                        <p class="card-text">A longer description for this example, showcasing a bigger card.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-3 mb-4">
+                <div class="card bg-dark text-white h-100">
+                    <img src="https://i.pinimg.com/736x/09/e0/0e/09e00e92e918ec475d261575cbad77e9.jpg" class="card-img" alt="corte prueba">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Card Title 4</h5>
+                        <p class="card-text">A compact card with minimal content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 mb-4">
+                <div class="card bg-dark text-white h-100">
+                    <img src="https://i.pinimg.com/736x/26/19/ee/2619ee8c219717b3b4c448895c98e235.jpg" class="card-img" alt="corte prueba">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Card Title 5</h5>
+                        <p class="card-text">This one has more space and stands out from the others.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Agrega más tarjetas según sea necesario -->
+        </div>
+    </div>
+</main>
+
+
+
+
+
+<?php require_once 'views/footer.php' ; ?>  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ barberiaWeb/views/header.php
@@ -0,0 +1,15 @@
+<header> 
+
+<nav class="navbar navbar-light bg-light">
+<div class="container-fluid">
+<a class="navbar-brand" href="#">
+  <img src="../assets/images/img_logo.jpg" alt="" width="30" height="24">
+</a>
+<form class="d-flex">
+  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+  <button class="btn btn-outline-success" type="submit">Search</button>
+</form>
+</div>
+</nav>
+
+</header>
